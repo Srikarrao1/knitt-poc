4a217aaae699df61aef090199c36df2ebd4d1ca0
Srikar <pasupuleti@antiersolutions.com>
2023-12-15 12:46:07 +0530

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [mnemonics,setMnemonics]=useState('')
+const [btcAmount,setBtcAmount]=useState(5)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className='form-container'>
+          <label>Mnemonics</label>
+          <textarea value={mnemonics} rows={6}  placeholder="Enter Mnemonics here" className='input-box-area' />
+          <p>Btc Address: 0xdgshfdsgfdgfgdfsg </p>
+
+          <p>Btc Balance: 100BTC </p>
+          <label>Amount</label>
+
+          <input value={btcAmount} placeholder="Enter BTC amount" className='input-box'></input>
+          <p>Eth Address: 0xdgshfdsgfdgfgdfsg</p>
+          <p>Eth Balance: 100ETH </p>
+
+          <p>You will get 2.0 wBTC</p>
+          <button className="swap-button">Swap</button>
+        </div>
+      </div>
     </div>
   );
 }
